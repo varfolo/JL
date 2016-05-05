1388c81ff767529fb599e49d87b5f96327dd3540
galaxy <galaxy@sumtech-pc.main.summatechnology.ru>
2016-05-05 15:29:29 +0300

    Exception

---
+++ LnkdLst/src/lnkdlst/Person.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lnkdlst;
+
+/**
+ *
+ * @author galaxy
+ */
+public class Person {
+    private String name;
+   
+    public Person(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+}
